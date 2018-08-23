756c3047817e06c4a98ee79b94ae4d4675831d55
Rion5 <chaigrindean@gmail.com>
2018-08-23 17:13:07 -0400

    Update: Parse JSON into an object and update the image with the parsed url

--- JS/RandomImageApp/app.js
+++ JS/RandomImageApp/app.js
@@ -1,5 +1,6 @@
 //Select button
 var btn = document.querySelector("#btn");
+var img = document.querySelector("#photo");
 
 //Listen for click
 btn.addEventListener("click", function(){
@@ -7,7 +8,11 @@ btn.addEventListener("click", function(){
     var XHR = new XMLHttpRequest();
     XHR.onreadystatechange = function(){
         if(XHR.readyState == 4 && XHR.status == 200){
-            console.log(XHR.responseText);
+            console.log(XHR.responseText);              //.responseText is a JSON string
+            var data = JSON.parse(XHR.responseText);    //Parse JSON into data obj
+            console.log(data.message);                  //Access data obj's message
+            var url = data.message;
+            img.src = url;
         }
     };
     XHR.open("GET","https://dog.ceo/api/breeds/image/random");
