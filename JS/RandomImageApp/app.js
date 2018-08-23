e7fb364653fb7adc6d7fff9beb37b08624776560
Rion5 <chaigrindean@gmail.com>
2018-08-23 17:05:38 -0400

    Update XMLHttpRequest

--- JS/RandomImageApp/app.js
+++ JS/RandomImageApp/app.js
@@ -4,4 +4,12 @@ var btn = document.querySelector("#btn");
 //Listen for click
 btn.addEventListener("click", function(){
     //Make a request
+    var XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function(){
+        if(XHR.readyState == 4 && XHR.status == 200){
+            console.log(XHR.responseText);
+        }
+    };
+    XHR.open("GET","https://dog.ceo/api/breeds/image/random");
+    XHR.send();
 });
