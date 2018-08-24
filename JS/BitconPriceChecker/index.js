ce0aafb59a278f8a8eb113c306298104cf72e950
Rion5 <chaigrindean@gmail.com>
2018-08-24 09:08:00 -0400

    Add XHR to btn click event listener

--- JS/BitconPriceChecker/index.js
+++ JS/BitconPriceChecker/index.js
@@ -5,5 +5,18 @@ var price = document.querySelector("#price");
 
 //Listen for click
 btn.addEventListener("click", function(){
-    alert("button clicked");
+    //Make a request
+    var XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = function(){
+        // 0 - Unsent, 1 - Opened, 2 - Headers received, 3 - Loading, 4 - Done
+        if(XHR.readyState == 4 && XHR.status == 200){
+            var parsedJSON = JSON.parse(XHR.responseText);
+            console.log(parsedJSON); //Entire parsed JSON object
+            console.log(parsedJSON.bpi.USD.rate);
+            price.textContent = "$"+parsedJSON.bpi.USD.rate;
+        }
+    };
+    XHR.open("GET","https://api.coindesk.com/v1/bpi/currentprice.json");
+    XHR.send();
 });
