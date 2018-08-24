c0188b1c0772e2022c1f169ebaa3d64e95a2825a
Rion5 <chaigrindean@gmail.com>
2018-08-24 12:42:04 -0400

    Update profile with retrieved value from API

--- JS/AJAX/RandomUserGenerator/index.js
+++ JS/AJAX/RandomUserGenerator/index.js
@@ -2,7 +2,9 @@
 var btn = document.querySelector("#btn");
 var fullNameDisplay = document.getElementById("fullname");
 var avatarDisplay = document.getElementById("avatar");
-
+var usernameDisplay = document.getElementById("username");
+var emailDisplay = document.getElementById("email");
+var cityDisplay = document.getElementById("city");
 //Click Event
 btn.addEventListener("click", function(){
     //Refactored fetch code will look similar to this
@@ -34,13 +36,16 @@ function updateProfile(data){
     console.log(data);      //data = parsed JSON object
     //Get values from parsed object (named data)
     //Values include, fullName, avatar, username, email, city
-    var fullName = data.results[0].name.first + " " + data.results[0].name.last;
-    var avatar = data.results[0].picture.large;
-    var username = data.results[0].login.username;
-    var email = data.results[0].email;
-    var city = data.results[0].location.city;
+    var fullName = data.name.first + " " + data.name.last;
+    var avatar = data.picture.large;
+    var username = data.login.username;
+    var email = data.email;
+    var city = data.location.city;
     fullNameDisplay.textContent = fullName;
     avatarDisplay.src = avatar;
+    usernameDisplay.textContent = username;
+    emailDisplay.textContent = email;
+    cityDisplay.textContent = city;
 
     console.log(fullName);
 }
