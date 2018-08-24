152095f6794c7d999d6a70e0ff60e6c8c4e4883d
Rion5 <chaigrindean@gmail.com>
2018-08-24 11:31:06 -0400

    Add non refactored code

--- JS/AJAX/RandomUserGenerator/index.js
+++ JS/AJAX/RandomUserGenerator/index.js
@@ -2,5 +2,19 @@
 var btn = document.querySelector("#btn");
 
 btn.addEventListener("click", function(){
-    
+    var url = "https://randomuser.me/api/";
+    //Refactored fetch code will look similar to this
+    // fetch(url)
+    // .then(handleErrors)
+    // .then(parseJSON)
+    // .then(updateProfile)
+    // .catch(error)
+
+    fetch(url)
+    .then(function(res){
+        console.log(res);
+    })
+    .catch(function(err){
+        console.log(err);
+    });
 });
