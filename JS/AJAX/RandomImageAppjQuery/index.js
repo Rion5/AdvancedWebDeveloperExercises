f4f3f8d5ad32e277cdcf84ca9d3a76d677fdd131
Rion5 <chaigrindean@gmail.com>
2018-08-28 16:51:22 -0400

    Add btn click event listener

--- JS/AJAX/RandomImageAppjQuery/index.js
+++ JS/AJAX/RandomImageAppjQuery/index.js
@@ -1,3 +1,8 @@
 //Select button
 var btn = document.querySelector("#btn");
 var img = document.getElementById("photo");
+
+//Click Event Listener
+btn.addEventListener("click",function(){
+    console.log("Button Click");
+});
