eaf87a293bd5d5b875eafdf47d5e9cde286bc01a
Rion5 <chaigrindean@gmail.com>
2018-08-28 16:59:41 -0400

    Switch DOM.getElement and querySelector method with jQuery

--- JS/AJAX/RandomImageAppjQuery/index.js
+++ JS/AJAX/RandomImageAppjQuery/index.js
@@ -1,8 +1,4 @@
-//Select button
-var btn = document.querySelector("#btn");
-var img = document.getElementById("photo");
-
-//Click Event Listener
-btn.addEventListener("click",function(){
-    console.log("Button Click");
+//jQuery Click Event
+$("#btn").click(function(){
+    console.log("Button Clicked");
 });
