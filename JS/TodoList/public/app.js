23cec7cd59ad045a7ae1a10bef1eec91b4f397b1
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-11-19 16:24:10 -0500

    Add app.js which contains jQuery code to load list from db, and display as list

---
+++ JS/TodoList/public/app.js
@@ -0,0 +1,16 @@
+// When DOM loads, send a request to /api/todos, and display the todos in a List
+$(document).ready( () =>{
+    $.getJSON("/api/todos")
+    .then(data => {
+        console.log(data);
+        addTodos(data);
+    });
+});
+
+function addTodos(todos){
+    todos.forEach(todo => {
+        console.log(todo.name);
+        var newTodo = $('<li class = "task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    });
+}
