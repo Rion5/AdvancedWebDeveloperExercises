8a7e503a539586bd6cfe7ae646e4a8cb1b3a2b44
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-11-19 16:11:51 -0500

    Add views/index.html and CSS stylesheet

--- JS/TodoList/index.js
+++ JS/TodoList/index.js
@@ -8,10 +8,12 @@ var todoRoutes = require('./routes/todos');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/todos', todoRoutes);
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 
 app.get("/", function(req, res){
-    res.send("Home Page");
+    res.sendFile("index.html");
 });
 
 //========================
